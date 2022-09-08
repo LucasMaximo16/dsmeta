42f3c5276405aff010b5cfee37ffdf504604dcd2
LucasMaximo16 <lucasmaximodeandrade@gmail.com>
2022-09-08 01:31:29 -0300

    Inserindo Estilos e Botão

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/notification-button"
 
 function App() {
-  return(<h1>Ola mundo</h1>)
+  return (
+    <>
+      <h1>Ola mundo</h1>
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
