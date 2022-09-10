6b471cf7e8a23745f7d260c7be724813b5c75a3f
LucasMaximo16 <lucasmaximodeandrade@gmail.com>
2022-09-10 17:07:54 -0300

    API test

---
+++ backend/dsmeta/src/main/java/com/devsuperior/dsmeta/repository/SaleRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dsmeta.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devsuperior.dsmeta.Entities.Sale;
+@Repository
+public interface SaleRepository extends JpaRepository<Sale,Long> {
+
+}
