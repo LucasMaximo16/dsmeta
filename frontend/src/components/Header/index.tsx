9662b48928b30a3dba7da9487f7342a866a587ee
LucasMaximo16 <lucasmaximodeandrade@gmail.com>
2022-09-12 02:19:43 -0300

    link

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,7 +9,7 @@ function Header() {
                 <h1>DSMeta</h1>
                 <p>
                     Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+                    <a href="linkedin.com/in/lucas-máximo-572b421b3">@devsuperior.ig</a>
                 </p>
             </div>
         </header>
